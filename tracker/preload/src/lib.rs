dcdda503f7d6d1fbdcde09cd274cee4e8cd87d2d
alexander.linne <alexander.linne@stud.uni-goettingen.de>
2019-03-08 21:46:26 +0100

    ADD: Travis CI

--- tracker/preload/src/lib.rs
+++ tracker/preload/src/lib.rs
@@ -7,8 +7,9 @@ mod tests {
     fn test_libc_preload() {
         let out_dir = env::var("OUT_DIR")
             .expect("OUT_DIR environment variable missing!");
-        let filepath = format!{"{}/test_libc_preload", out_dir};
-        let output = Command::new(filepath)
+        let command = format!{"cd {} && ./test_libc_preload", out_dir};
+        let output = Command::new("bash")
+            .args(&["-c", command.as_str()])
             .stdout(Stdio::inherit())
             .stderr(Stdio::inherit())
             .output()
